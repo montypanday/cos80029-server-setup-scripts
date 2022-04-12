77b9e643f129f4917bfe279c6928c7e6fa2c5dd6
Monty Panday <montypanday293@gmail.com>
2022-04-12 22:33:36 +1000

    idp file started
    Need to know if we start idp in dev mode

---
+++ idp.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install default-jdk
+
+cd /opt
+sudo wget https://github.com/keycloak/keycloak/releases/download/17.0.1/keycloak-17.0.1.tar.gz
+sudo tar -xvzf keycloak-17.0.1.tar.gz
