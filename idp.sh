58dffb0421c29c7858e89893195ef3101010ebda
Monty Panday <montypanday293@gmail.com>
2022-05-20 00:30:57 +1000

    configure keycloak commands

--- idp.sh
+++ idp.sh
@@ -13,10 +13,17 @@ git clone https://github.com/MBKillall5/wpsso.git
 
 cd wpsso
 cd wp-federation
+sudo apt install maven
 mvn -pl custom-wordpress-federation -am clean install
-
-sudo cp /home/student/wpsso/wp-federation/custom-wordpress-federation/target/custom-wordpress-federation-1.0.0.0.jar /opt/keycloak-17.0.0/providers
+cd custom-wordpress-federation
+sudo mv /target/custom-wordpress-federation-1.0.0.0-SNAPSHOT.jar /opt/keycloak-17.0.1/providers
 
 cd /opt/keycloak-17.0.1
 bin/kc.sh build
 sudo bin/kc.sh start-dev --hostname idp.com
+
+cd /bin
+kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
+kcadm.sh create realms -s realm=entrustict -s enabled=true -o
+kcadm.sh create clients -r entrustict -s clientId=http://172.20.200.2/wp-content/plugins/miniorange-saml-20-single-sign-on/ -s enabled=true
+kcadm.sh create clients -r entrustict -s clientId=http://172.20.200.3/wp-content/plugins/miniorange-saml-20-single-sign-on/ -s enabled=true
